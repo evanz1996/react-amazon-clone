500a5e0b158a908990522839292400d831b5db27
Vanessa Valenzuela <vanessavalenzuela08@gmail.com>
2022-02-02 01:10:05 +0800

    2nd commit
    2nd commit

---
+++ react-amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDUwoCIBPjviu4ccRMU_vQVTIXsdgYA2ys',
+  authDomain: 'clone-f7244.firebaseapp.com',
+  projectId: 'clone-f7244',
+  storageBucket: 'clone-f7244.appspot.com',
+  messagingSenderId: '56233424112',
+  appId: '1:56233424112:web:cc8a4ffa8b0be2fd0cbe35',
+  measurementId: 'G-886J09435P',
+};
