04eb29690181311f067a5ddeb9edc4aa732ff3e5
Vanessa Valenzuela <vanessavalenzuela08@gmail.com>
2022-02-03 01:55:54 +0800

    subtotal, checkout, route
    subtotal, checkout and route

--- react-amazon-clone/src/App.js
+++ react-amazon-clone/src/App.js
@@ -2,12 +2,20 @@ import './App.css';
 import Header from './Header';
 import Home from './Home';
 import 'react-router-dom';
-import { Router } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
+import Checkout from './Checkout';
 function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
+      <Switch>
+        <Route path="/checkout">
+          <Checkout />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
