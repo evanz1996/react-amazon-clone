8aaab46623040ee47c35ffbb58ad91522d06dd46
Vanessa Valenzuela <vanessavalenzuela08@gmail.com>
2022-02-03 00:16:36 +0800

    display products
    display products

--- react-amazon-clone/src/App.js
+++ react-amazon-clone/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import 'react-router-dom';
+import { Router } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
--- react-amazon-clone/src/Home.js
+++ react-amazon-clone/src/Home.js
@@ -12,21 +12,51 @@ function Home() {
 
         <div className="home__row">
           <Product
-            title="The Learn Startup"
+            id="23445930"
+            title="Tablet"
+            price={29.99}
+            image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
+            rating={5}
+          />
+          <Product
+            id="49538094"
+            title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk"
+            price={293.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/810%2BGNdkzKL._AC_SX450_.jpg"
+            rating={4}
+          />
+        </div>
+        <div className="home__row">
+          <Product
+            id="3254354345"
+            title="The LXFgr"
             price={29.99}
             image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
             rating={5}
           />
-          <Product />
+          <Product
+            id="4903850"
+            title="The apple watch"
+            price={199.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
+            rating={5}
+          />
+          <Product
+            id="3254354345"
+            title="New Apple Ipad Pro 12.9-inch, Wi-Fi, 128GB"
+            price={598.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
+            rating={4}
+          />
         </div>
         <div className="home__row">
-          <Product />
-          <Product />
-          <Product />
-        </div>
-        <div className="home__row">
-          {' '}
-          <Product />
+          <Product
+            id="90829332"
+            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
+            price={1094.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg"
+            rating={4}
+          />
         </div>
       </div>
     </div>
